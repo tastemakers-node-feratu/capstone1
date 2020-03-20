471aea2e1a89fd9bf8d952eaf8ff62253df06c9b
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-03-20 13:40:14 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// logging middleware
+app.use(morgan('dev'));
+// body parsing middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// static middleware
+app.use(express.static(path.join(__dirname, './public')));
+
+app.use('/api', require('./api'));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// error handling: 500 internal server error
+app.use(function(err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+// use port 3000 for heroku
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`Your server, listening on port ${port}`);
+});
