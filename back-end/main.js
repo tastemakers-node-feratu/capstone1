348b74dc8a13a2721db7e63fd9cfa24ceeb4a40a
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2020-03-21 16:53:40 -0400

    fix: typo in seed file

---
+++ back-end/main.js
@@ -0,0 +1,12 @@
+const db = require('./server/db');
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+db.sync({force: true}) // if you update your db schemas, make sure you drop the tables first and then recreate them
+  .then(() => {
+    console.log('db synced');
+    app.listen(PORT, () =>
+      console.log(`studiously serving silly sounds on port ${PORT}`)
+    );
+  });
