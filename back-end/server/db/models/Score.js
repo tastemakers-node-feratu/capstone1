bbcf6b15e118d6cfaef54f2dcc10fa61c9f0d786
Isabel Jao <isabeljao@gmail.com>
2020-04-06 18:48:28 -0400

    Update: score table is updating properly

--- back-end/server/db/models/Score.js
+++ back-end/server/db/models/Score.js
@@ -5,29 +5,35 @@ const Score = db.define('score', {
   totalScore: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
-    set(value) {
-      if (this.totalScore) {
-        return this.setDataValue('totalScore', value + this.totalScore);
-      }
-      return this.setDataValue('totalScore', value);
-    }
+    // set(value) {
+    //   if (this.totalScore) {
+    //     return this.setDataValue('totalScore', value + this.totalScore);
+    //   }
+    //   return this.setDataValue('totalScore', value);
+    // }
   },
   averageScore: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
-    set() {
-      const newValue = Math.round(this.totalScore / this.counter);
-      return this.setDataValue('averageScore', newValue);
-    }
+    // set() {
+    //   const newValue = Math.round(this.totalScore / this.counter);
+    //   return this.setDataValue('averageScore', newValue);
+    // }
   },
   counter: {
     type: Sequelize.INTEGER,
     defaultValue: 1,
-    set() {
-      const newCounterValue = this.counter + 1;
-      return this.setDataValue('counter', newCounterValue);
-    }
+    // set() {
+    //   const newCounterValue = this.counter + 1;
+    //   return this.setDataValue('counter', newCounterValue);
+    // }
   }
 });
 
 module.exports = Score;
+
+Score.beforeSave((score, options) => {
+  score.counter += 1
+  score.averageScore = Math.round(score.totalScore / score.counter);
+  // console.log('NEW HOOOK', score.averageScore)
+});
